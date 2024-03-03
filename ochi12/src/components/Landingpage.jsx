53e6d25018b53f685a2ec453e7fbc75b8833fa7c
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-03-04 00:28:08 +0530

    Create Landingpage.jsx

---
+++ ochi12/src/components/Landingpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Landingpage() {
+  return (
+    <div className='w-full h-screen bg-zinc-900'>
+        <div className="textstructure">
+            <div className='masker '>
+                <h1 className='uppercaseTest font-["Founders Grotesk X-Cond SmBd"]'>We Create</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Landingpage
