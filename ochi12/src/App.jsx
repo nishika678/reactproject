c5fcbed3a9d638f2a8b945e46a8e4be5b2e5542d
nishika678 <147605762+nishika678@users.noreply.github.com>
2024-03-04 00:28:18 +0530

    Create App.jsx

---
+++ ochi12/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Landingpage from './components/Landingpage'
+
+function App() {
+  return (
+    <div className='w-full h-screen text-white'>
+      <Navbar />
+      <Landingpage />
+    </div>
+  )
+}
+
+export default App
